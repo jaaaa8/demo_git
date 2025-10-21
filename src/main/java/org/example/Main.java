4f6889c7aed4cda4e6f4d542aa7ada4f9a729e9b
jaaaa8 <bileyghas1eye@gmail.com>
2025-10-21 20:06:39 +0700

    package TriDepTrai

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     }
     public static void main(String[] args) {
         System.out.println(add(10, 20));
-       sub(5,6);
+        sub(5,6);
     }
     public static void sub(int a, int b){
         System.out.println(a-b);
